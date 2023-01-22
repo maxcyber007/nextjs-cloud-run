caff260f41fe60070e4b1b2f90c32f9783ce3ed9
Anuchat Rungsiyanon <rungsiyanon@gmail.com>
2023-01-22 13:52:24 +0700

    18

--- next.config.js
+++ next.config.js
@@ -7,7 +7,17 @@ const nextConfig = {
 
 module.exports = nextConfig
 */
-/** @type {import('next').NextConfig} */
+/*
 module.exports = {
   output: 'standalone',
+}
+*/
+module.exports = {
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 }
